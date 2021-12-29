aaa929cd0f8f4e436b1fe856cc429fb9acfac07f
Jerell Kuo <jkfox67@gmail.com>
2021-12-30 05:30:50 +0800

    init commit

---
+++ pages/advertise/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="red">
+    Hello {{ name }}!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'AdvertiseManage',
+  data () {
+    return {
+      name: 'hey'
+    }
+  }
+}
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
